283564b00e16a31b0b907b783e9f0a57f129392f
Johan Romeo <jofiluren@outlook.com>
2024-02-19 19:06:24 +0100

    fixed Internship Description overflow:'auto'

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Internship Management Frontend
 
-+ This is the client for calling: [InternshipManagement](https://github.com/JohRome/InternshipManagement)
++ This is the client for calling:[InternshipManagement](https://github.com/JohRome/InternshipManagement)
 + This frontend is build by a guide by JavaGuide found here: https://www.youtube.com/playlist?list=PLGRDMO4rOGcODJeYSY08lIILkqoydQI2k
 + Changes were made to fit my backend.
--- ims-frontend/src/components/ListInternshipComponent.jsx
+++ ims-frontend/src/components/ListInternshipComponent.jsx
@@ -1,52 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import { deleteInternship, listInternships } from '../services/InternshipService'
-import { useNavigate } from 'react-router-dom'
-
+import React, { useEffect, useState } from 'react';
+import { deleteInternship, listInternships } from '../services/InternshipService';
+import { useNavigate } from 'react-router-dom';
 
 const ListInternshipComponent = () => {
-
-    const [internships, setInternships] = useState([])
-
+    const [internships, setInternships] = useState([]);
     const navigator = useNavigate();
 
     useEffect(() => {
         getAllInternships();
-
-    }, [])
+    }, []);
 
     function getAllInternships() {
-        listInternships().then((response) => {
-            setInternships(response.data);
-        }).catch(error => {
-            console.error(error);
-        })
+        listInternships()
+            .then((response) => {
+                setInternships(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     function addNewInternship() {
-        navigator('/add-internship')
+        navigator('/add-internship');
     }
 
     function updateInternship(id) {
-        navigator(`/edit-internship/${id}`)
+        navigator(`/edit-internship/${id}`);
     }
 
     function removeInternship(id) {
         console.log(id);
 
-        deleteInternship(id).then((response) => {
-            getAllInternships();
-
-        }).catch(error => {
-            console.error(error);
-        })
+        deleteInternship(id)
+            .then((response) => {
+                getAllInternships();
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     return (
-        <div className='container'>
-
-            <h2 className='text-center'>List of Internships</h2>
-            <button className='btn btn-primary mb-2' onClick={addNewInternship}>Add Internship</button>
-            <table className='table table-light table-bordered'>
+        <div className="container">
+            <h2 className="text-center">List of Internships</h2>
+            <button className="btn btn-primary mb-2" onClick={addNewInternship}>
+                Add Internship
+            </button>
+            <table className="table table-light table-bordered">
                 <thead>
                     <tr>
                         <th>Company Id</th>
@@ -58,30 +58,31 @@ const ListInternshipComponent = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {
-                        internships.map(internship =>
-                            <tr key={internship.id}>
-                                <td>{internship.id}</td>
-                                <td>{internship.companyName}</td>
-                                <td>{internship.description}</td>
-                                <td>{internship.contactPerson}</td>
-                                <td>{internship.hasApplied ? "Yes" : "No"}</td>
-                                <td>
-                                    <button className='btn btn-info' onClick={() => updateInternship(internship.id)}>Update</button>
-                                    <button className='btn btn-danger' onClick={() => removeInternship(internship.id)}
-                                        style={{ marginLeft: '10px' }}
-                                    >Delete</button>
-                                </td>
-                            </tr>)
-                    }
-                    <tr>
-
-                    </tr>
+                    {internships.map((internship) => (
+                        <tr key={internship.id}>
+                            <td>{internship.id}</td>
+                            <td>{internship.companyName}</td>
+                            <td style={{ maxWidth: '300px', overflow: 'auto' }}>{internship.description}</td>
+                            <td>{internship.contactPerson}</td>
+                            <td>{internship.hasApplied ? 'Yes' : 'No'}</td>
+                            <td>
+                                <button className="btn btn-info" onClick={() => updateInternship(internship.id)}>
+                                    Update
+                                </button>
+                                <button
+                                    className="btn btn-danger"
+                                    onClick={() => removeInternship(internship.id)}
+                                    style={{ marginLeft: '10px' }}
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
-
         </div>
-    )
-}
+    );
+};
 
-export default ListInternshipComponent
+export default ListInternshipComponent;
